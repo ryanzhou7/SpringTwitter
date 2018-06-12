ae8b5c86bf1709408cbffef28cd772b250bb8e0d
Ryan <Ryanzhou7@gmail.com>
2018-06-12 11:07:59 -0700

    Create spring project

---
+++ src/main/java/com/ryanzhou/SpringTweetsApplication.java
@@ -0,0 +1,12 @@
+package com.ryanzhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTweetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTweetsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ryanzhou/SpringTweetsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ryanzhou;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTweetsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
