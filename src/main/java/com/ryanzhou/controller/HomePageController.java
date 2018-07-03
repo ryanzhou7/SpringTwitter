f2f7e10b362d25f0717bbd94944f10de62fdd5d6
Ryan <Ryanzhou7@gmail.com>
2018-07-03 10:15:06 -0700

    update app properties

--- src/main/java/com/ryanzhou/controller/HomePageController.java
+++ src/main/java/com/ryanzhou/controller/HomePageController.java
@@ -1,8 +1,5 @@
 package com.ryanzhou.controller;
 
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomePageController {
 	
 	private String message = "Hello, please add '/swagger-ui.html#' "
-			+ "to the end of the url to view the swagger documentation for the api!";
-
+			+ "to the end of the url to view the swagger documentation for the api or click below";
+	
 	@GetMapping("/")
 	public String welcome(Model model) {
 		model.addAttribute("message", message);
